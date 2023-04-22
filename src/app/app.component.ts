61829ea0b59d0789ecc3ea9da33e9c0fd1d714d9
laseronline <razeronline@hotmail.com>
2023-04-22 23:58:31 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular_localStorage_tutorial';
+  session: any;
+
+  title = 'Angular localstorage ';
+
+  savaData() {
+    let data = { id: 1, name: 'localstorage ' };
+    localStorage.setItem('session', JSON.stringify(data));
+  }
+  loadData() {
+    let data: any = localStorage.getItem('session');
+    this.session = JSON.parse(data);
+    alert(data);
+  }
+  removeData() {
+    localStorage.removeItem('session');
+    this.session = null;
+  }
 }
